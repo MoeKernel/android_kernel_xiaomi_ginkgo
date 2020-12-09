8fc806e89c22e7f44c053ebb8389e6d6f266fbb7
Gao Xiang <hsiangkao@redhat.com>
2020-12-09 20:37:17 +0800

    erofs: force inplace I/O under low memory scenario
    Try to forcely switch to inplace I/O under low memory scenario in
    order to avoid direct memory reclaim due to cached page allocation.
    
    Link: https://lore.kernel.org/r/20201209123717.12430-1-hsiangkao@aol.com
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Gao Xiang <hsiangkao@redhat.com>
    Change-Id: I8ea2d3b59c68125271f66853cf5dc6ca39e7aaa9
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- fs/erofs/compress.h
+++ fs/erofs/compress.h
@@ -25,10 +25,28 @@ struct z_erofs_decompress_req {
 	bool inplace_io, partial_decoding;
 };
 
+/* some special page->private (unsigned long, see below) */
+#define Z_EROFS_SHORTLIVED_PAGE		(-1UL << 2)
+#define Z_EROFS_PREALLOCATED_PAGE	(-2UL << 2)
+
 /*
- * - 0x5A110C8D ('sallocated', Z_EROFS_MAPPING_STAGING) -
- * used to mark temporary allocated pages from other
- * file/cached pages and NULL mapping pages.
+ * For all pages in a pcluster, page->private should be one of
+ * Type                         Last 2bits      page->private
+ * short-lived page             00              Z_EROFS_SHORTLIVED_PAGE
+ * preallocated page (tryalloc) 00              Z_EROFS_PREALLOCATED_PAGE
+ * cached/managed page          00              pointer to z_erofs_pcluster
+ * online page (file-backed,    01/10/11        sub-index << 2 | count
+ *              some pages can be used for inplace I/O)
+ *
+ * page->mapping should be one of
+ * Type                 page->mapping
+ * short-lived page     NULL
+ * preallocated page    NULL
+ * cached/managed page  non-NULL or NULL (invalidated/truncated page)
+ * online page          non-NULL
+ *
+ * For all managed pages, PG_private should be set with 1 extra refcount,
+ * which is used for page reclaim / migration.
  */
 #define Z_EROFS_MAPPING_STAGING         ((void *)0x5A110C8D)
 
