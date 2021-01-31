a53418f3c7fee506e552d457d6f0823c759bf554
Frederic Weisbecker <frederic@kernel.org>
2021-02-01 00:05:44 +0100

    rcu: Pull deferred rcuog wake up to rcu_eqs_enter() callers
    Deferred wakeup of rcuog kthreads upon RCU idle mode entry is going to
    be handled differently whether initiated by idle, user or guest. Prepare
    with pulling that control up to rcu_eqs_enter() callers.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: stable@vger.kernel.org
    Link: https://lkml.kernel.org/r/20210131230548.32970-2-frederic@kernel.org
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -595,7 +595,6 @@ static void rcu_eqs_enter(bool user)
 	trace_rcu_dyntick(TPS("Start"), rdp->dynticks_nesting, 0, atomic_read(&rdp->dynticks));
 	WARN_ON_ONCE(IS_ENABLED(CONFIG_RCU_EQS_DEBUG) && !user && !is_idle_task(current));
 	rdp = this_cpu_ptr(&rcu_data);
-	do_nocb_deferred_wakeup(rdp);
 	rcu_prepare_for_idle();
 	rcu_preempt_deferred_qs(current);
 	WRITE_ONCE(rdp->dynticks_nesting, 0); /* Avoid irq-access tearing. */
@@ -621,7 +620,10 @@ void rcu_idle_enter(void)
 
 	lockdep_assert_irqs_disabled();
 	do_nocb_deferred_wakeup(rdp);
+	struct rcu_data *rdp = this_cpu_ptr(&rcu_data);
+
 	lockdep_assert_irqs_disabled();
+	do_nocb_deferred_wakeup(rdp);
 	rcu_eqs_enter(false);
 }
 
@@ -648,7 +650,13 @@ void rcu_user_enter(void)
 	do_nocb_deferred_wakeup(rdp);
 	instrumentation_end();
 
+	struct rcu_data *rdp = this_cpu_ptr(&rcu_data);
 	lockdep_assert_irqs_disabled();
+
+	instrumentation_begin();
+	do_nocb_deferred_wakeup(rdp);
+	instrumentation_end();
+
 	rcu_eqs_enter(true);
 }
 #endif /* CONFIG_NO_HZ_FULL */
