99f1d664ea632bb1546c527fe910dcdad02f3f0c
akari <akariondev@gmail.com>
2024-04-18 14:22:41 +0000

    Revert "Import simonpunk's SUSFS"
    This reverts commit ef03147a1cad2523695b8083dcc6682b75e14612.

--- fs/stat.c
+++ fs/stat.c
@@ -22,11 +22,6 @@
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
 
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_SUSFS)
-#include <linux/susfs.h>
-#endif
-
-
 /**
  * generic_fillattr - Fill in the basic attributes from the inode struct
  * @inode: Inode to use as the source
@@ -116,11 +111,9 @@ int vfs_getattr(const struct path *path, struct kstat *stat,
 {
 	int retval;
 
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_SUSFS)
-	if (susfs_is_suspicious_path(path, &retval, SYSCALL_FAMILY_ALL_ENOENT)) {
-		return retval;
+	if (is_suspicious_path(path)) {
+		return -ENOENT;
 	}
-#endif
 
 	retval = security_inode_getattr(path);
 	if (retval)
@@ -346,9 +339,6 @@ static int cp_new_stat(struct kstat *stat, struct stat __user *statbuf)
 #endif
 	tmp.st_blocks = stat->blocks;
 	tmp.st_blksize = stat->blksize;
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_SUSFS)
-	susfs_suspicious_kstat(tmp.st_ino, &tmp);
-#endif
 	return copy_to_user(statbuf,&tmp,sizeof(tmp)) ? -EFAULT : 0;
 }
 
