7b4bb0a9b4b63c5108e29fa9ea828793229a85e5
Akari <barryofc11@gmail.com>
2024-05-08 01:56:19 +0000

    ksu_update.sh: update
    Signed-off-by: Akari <barryofc11@gmail.com>

--- ksu_update.sh
+++ ksu_update.sh
@@ -4,30 +4,19 @@
 #   By Akari Nyan - © 2023
 # ----------------------------- 
 
-# Sets the default option
 tag="stable"
 
-# Sets the default option
-# to remove the KPROBES warning
-remove_kprobes_warning="y"
-
-# Process command line arguments
-while getopts "t:k:h" opt; do
+while getopts "t:h" opt; do
   case $opt in
     t)
       tag="$OPTARG"
       ;;
-    k)
-      remove_kprobes_warning="$OPTARG"
-      ;;
     h)
-      echo "Use: ./ksu_update.sh [-t <tag>] [-k <remove warning KPROBES>] [-h]"
+      echo "Use: ./ksu_update.sh [-t <tag>] [-h]"
       echo ""
       echo "Options:"
       echo "  -t <tag> Selects the KernelSU tag. Options: stable (default), dev (unstable)"
-      
-      echo "  -k <remove warning KPROBES> Whether to remove. Options: y, n (y: default)"
-
+  
       echo "  -h, --help Display this help message"
       exit 0
       ;;
@@ -38,7 +27,6 @@ while getopts "t:k:h" opt; do
   esac
 done
 
-# Defines the command to be executed based on the selected tag
 if [ "$tag" = "stable" ]; then
   rm -rf KernelSU
   cmd="bash -"
@@ -46,15 +34,9 @@ elif [ "$tag" = "dev" ]; then
   rm -rf KernelSU
   cmd="bash -s main"
 else
-  echo "Tag inválida: $tag" >&2
+  echo "tag invalid: $tag" >&2
   exit 1
 fi
 
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | $cmd
 
-# Remove KPROBES dependency warning 
-# if option is set to "y"
-# Default: y (yes)
-if [ "$remove_kprobes_warning" = "y" ]; then
-  sed -i '59,60d' KernelSU/kernel/ksu.c
-fi
