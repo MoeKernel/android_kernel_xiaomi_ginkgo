435abbd45906e888b47f9328f7fb414ef8ef804e
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-11-21 01:59:51 +0800

    /proc/pid/maps: dont allow paths with lineage to be listed
    Change-Id: I2741d1d9763e8233212db0df2e77f990f1ea9291

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -656,13 +656,7 @@ bypass_orig_flow:
         struct dentry *dentry = file->f_path.dentry;
         if (dentry) {
         	const char *path = (const char *)dentry->d_name.name; 
-            	if (strstr(path, "lineage")) { 
-	  	start = vma->vm_start;
-		end = vma->vm_end;
-		show_vma_header_prefix_fake(m, start, end, flags, pgoff, dev, ino);
-            	name = "/system/framework/framework-res.apk";
-		goto done;
-            	 	}
+            	if (strstr(path, "lineage")) { return; }
             	}
 	}
 
