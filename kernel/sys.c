3f1da8780088fc4732165a57099d1e9ce3513b55
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2025-08-19 14:21:15 +0000

    syscall: Sync uname with android12-5.4
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1227,12 +1227,10 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 		!strncmp(current->comm, "netbpfload", 10) ||
 		!strncmp(current->comm, "uprobestatsbpfload", 18) ||
-	    !strncmp(current->comm, "netd", 4))) {
-		if (current_uid().val == 0) {
-			strcpy(tmp.release, "5.4.186");
-			pr_debug("fake uname: %s/%d release=%s\n",
-				 current->comm, current->pid, tmp.release);
-		}
+	    !strncmp(current->comm, "netd", 4)) {
+		strcpy(tmp.release, "5.4.290");
+		pr_debug("fake uname: %s/%d release=%s\n",
+			current->comm, current->pid, tmp.release);
 	}
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
