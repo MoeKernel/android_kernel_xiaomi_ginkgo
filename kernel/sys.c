42b2469abc479eede9897039ce33be5a2e074e94
Shoiya Akari <barryofc11@gmail.com>
2025-08-31 21:47:38 +0000

    kernel: sys: fix error extraneous closing brace }
    Signed-off-by: Shoiya Akari <barryofc11@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1215,13 +1215,6 @@ static int override_release(char __user *release, size_t len)
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 extern void susfs_spoof_uname(struct new_utsname* tmp);
 #endif
-
-	return ret;
-}
-
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern void susfs_spoof_uname(struct new_utsname* tmp);
-#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
