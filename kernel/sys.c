cef17c7653619e9415e2640809dee2d7274cb94b
Shoiya Akari <barryofc11@gmail.com>
2025-08-31 22:37:15 +0000

    kernel: sys: fix error build
    Signed-off-by: Shoiya Akari <barryofc11@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1220,10 +1220,8 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-	susfs_spoof_uname(&tmp);
-#endif
 	memcpy(&tmp, utsname(), sizeof(tmp));
+
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 		!strncmp(current->comm, "netbpfload", 10) ||
 		!strncmp(current->comm, "uprobestatsbpfload", 18) ||
@@ -1261,13 +1259,6 @@ SYSCALL_DEFINE1(uname, struct old_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (!strncmp(current->comm, "bpfloader", 9) ||
-		!strncmp(current->comm, "netbpfload", 10) ||
-		!strncmp(current->comm, "netd", 4)) {
-			strcpy(tmp.release, "4.19.1"); // fake 4.19
-			pr_err("faking kernel version: %s/%d release=%s\n",
-				current->comm, current->pid, tmp.release);
-	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
