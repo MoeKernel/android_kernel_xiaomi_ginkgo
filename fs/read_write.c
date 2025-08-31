db14da4ebec85fea0838525dcdf024ef30c2701c
Shoiya Akari <barryofc11@gmail.com>
2025-08-31 22:03:23 +0000

    fs: read_write: drop ksu_handle_vfs_read
    Signed-off-by: Shoiya Akari <barryofc11@gmail.com>

--- fs/read_write.c
+++ fs/read_write.c
@@ -433,11 +433,6 @@ ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
 
-	#ifdef CONFIG_KSU
-	if (unlikely(ksu_vfs_read_hook))
-		ksu_handle_vfs_read(&file, &buf, &count, &pos);
-	#endif
-
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
 	if (!(file->f_mode & FMODE_CAN_READ))
